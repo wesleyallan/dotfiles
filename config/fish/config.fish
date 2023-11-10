dc0835b37328508d75440b5c7366d12895edef89
Wesley Silva <wesley.allansilva@gmail.com>
2023-11-09 23:17:28 -0300

    chore: Adjust scripts

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,20 @@
+## ███████╗███████╗████████╗
+## ██╔════╝██╔════╝╚══██╔══╝
+## ███████╗█████╗     ██║
+## ╚════██║██╔══╝     ██║
+## ███████║███████╗   ██║
+## ╚══════╝╚══════╝   ╚═╝
+
 set fish_greeting
 set -gx PATH $HOME/.local/bin:$HOME/.cargo/bin:$PATH
 set -g IFS \n\ \t\
+
+##  █████╗ ██╗     ██╗ █████╗ ███████╗
+## ██╔══██╗██║     ██║██╔══██╗██╔════╝
+## ███████║██║     ██║███████║███████╗
+## ██╔══██║██║     ██║██╔══██║╚════██║
+## ██║  ██║███████╗██║██║  ██║███████║
+## ╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═╝╚══════╝
 
 alias ls='exa -al --color=always --group-directories-first --icons' # my preferred listing
 alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
@@ -11,6 +25,13 @@ alias l.='exa -a | egrep "^\."'
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
+
+## ██████╗ ██╗   ██╗███╗   ██╗
+## ██╔══██╗██║   ██║████╗  ██║
+## ██████╔╝██║   ██║██╔██╗ ██║
+## ██╔══██╗██║   ██║██║╚██╗██║
+## ██║  ██║╚██████╔╝██║ ╚████║
+## ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
 
 source $HOME/.asdf/asdf.fish
 starship init fish | source
